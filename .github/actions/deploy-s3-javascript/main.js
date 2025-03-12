b42acdfe9c212c6bd1ac4227b65ad285e832da6b
Michal Kobylinski <michal.kobylinski@intel.com>
2025-03-12 18:52:33 +0100

    fix js custom action 3

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -2,6 +2,8 @@ import * as core from '@actions/core';
 import github from '@actions/github';
 import exec from '@actions/exec';
 
+Object.assign(global, core);
+
 function run() {
     core.notice('Hello from my custom js action');
 }
