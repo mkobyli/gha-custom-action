dbf85b635c35cfe7662ba17c45a4ea87e3040a74
Michal Kobylinski <michal.kobylinski@intel.com>
2025-03-12 19:32:08 +0100

    updated js custom action

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -4,6 +4,16 @@ const exec = require('@actions/exec');
 
 function run() {
     core.notice('Hello from my custom js action');
+
+    const bucket = core.getInput('bucket', {required: true})
+    const bucketRegion = core.getInput('bucket-region', {required: true})
+    const distFolder = core.getInput('dist-folder', {required: true})
+
+    const ctxSha = github.context.sha
+    exec.exec(`echo sha ${ctxSha}`)
+
+    const s3Uri = `s3://${bucket}`
+    exec.exec(`echo aws ${distFolder} ${s3Uri} --region ${bucketRegion}`)
 }
 
 run();
