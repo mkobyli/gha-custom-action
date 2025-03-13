9db0c8ccada6aad18da1af0d2765576268f9bc51
Michal Kobylinski <michal.kobylinski@intel.com>
2025-03-13 18:27:54 +0100

    docker action 7

--- .github/actions/deploy-docker/deployment.py
+++ .github/actions/deploy-docker/deployment.py
@@ -6,13 +6,25 @@ def create_tag():
     tag_name = os.environ['INPUT_TAG-NAME']
     extra_path = os.environ['INPUT_REPO-PATH']
     repo_path =  os.environ['GITHUB_WORKSPACE']
+
+    # Prepare path to repo
     if extra_path != "":
         repo_path = repo_path + '/' + extra_path
     print("repo path: ", repo_path)
+
+    # Create repo object
     repo = Repo(repo_path)
+
+    # Get the Git command object
+    git_cmd = repo.git
+
+    # Add the directory to the list of safe directories
+    git_cmd.config('--global', '--add', 'safe.directory', repo_path)
+    print(f"Added {repo_path} to safe directories.")
+
     last_commit = repo.head.commit
-    # print("Last commit message:", last_commit.message)
-    # print("Last commit SHA:", last_commit.hexsha)
+    print("Last commit message:", last_commit.message)
+    print("Last commit SHA:", last_commit.hexsha)
 
     new_tag = tag_name
     with open(os.environ['GITHUB_OUTPUT'], 'a') as gh_output:
